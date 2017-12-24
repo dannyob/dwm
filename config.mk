ec2469d57ac8a1e1c2ee98e8185b4890d0d7f6db
Danny O'Brien <danny@spesh.com>
2017-12-23 17:45:31 -0800

    Local install location.

--- config.mk
+++ config.mk
@@ -4,7 +4,8 @@ VERSION = 6.1
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+HOME = /home/danny
+PREFIX = ${HOME}/.local
 MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
