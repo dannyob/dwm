1e08105b3c7ccd3c682bb7ecc420aeb2a61e11cb
Danny O'Brien <danny@spesh.com>
2018-10-07 21:17:02 -0700

    Switch to nerdfonts.

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "iosevka term:size=8" };
+static const char *fonts[]          = { "Iosevka Nerd Font Mono:size=8", "iosevka term:size=8" };
 static const char dmenufont[]       = "iosevka term:size=8";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
